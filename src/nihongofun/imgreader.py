d3d0fea5f7b96b8a2c69779b15c766489cc92cf9
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-05-30 15:46:14 +0200

    Feat: Declare Main and file fetching methods

--- src/nihongofun/imgreader.py
+++ src/nihongofun/imgreader.py
@@ -1,1 +1,15 @@
 import easyocr
+import tkinter
+from tkinter import filedialog
+
+
+def fetch_file() -> str:
+    return "Hello"
+
+
+def main():
+    print("Hello")
+
+
+if __name__ == "__main__":
+    main()
