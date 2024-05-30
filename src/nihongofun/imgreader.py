17f277b9929cfbcda56b010c1e08bed77a8404ff
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-05-30 15:55:48 +0200

    Feat: Implement filepath fetching function

--- src/nihongofun/imgreader.py
+++ src/nihongofun/imgreader.py
@@ -1,14 +1,24 @@
 import easyocr
-import tkinter
 from tkinter import filedialog
 
 
 def fetch_file() -> str:
-    return "Hello"
+    print("Please choose an image file for text processing!")
+    file_path = filedialog.askdirectory()
+
+    while not file_path.lower().endswith((".png", ".jpg")):
+        response = input("No existing file at given path, try again? y/n")
+
+        if response.lower() == "n":
+            file_path = "QUIT!"
+            break
+        else:
+            file_path = filedialog.askdirectory()
+    return file_path
 
 
 def main():
-    print("Hello")
+    print(fetch_file())
 
 
 if __name__ == "__main__":
